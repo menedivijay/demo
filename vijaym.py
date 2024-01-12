6de6b53a3fa7318a703a96f3ff6039b84d7d44cb
menedivijay <143534233+menedivijay@users.noreply.github.com>
2024-01-12 10:50:00 +0530

    Update vijaym.py

--- vijaym.py
+++ vijaym.py
@@ -1,4 +1,4 @@
 import streamlit as st
 st.header('puppy')
 st.write('puppy')
-st.image('https://i.pining.com/564x/36/b4/ef/36b4ef433ac9c2cf9d34e54aa3a38340.jpg',caption='puppy')
+st.image('https://png.pngtree.com/background/20230528/original/pngtree-cute-puppy-wallpapers-picture-image_2774290.jpg',caption='puppy')
